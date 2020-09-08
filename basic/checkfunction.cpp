162afd1de68413d8aa5ea4ba625b6f72c7525560
xiuguodong <1811609338@qq.com>
2020-09-08 15:16:33 +0800

    修复优化判断问题，对棋面判断更加准确

--- basic/checkfunction.cpp
+++ basic/checkfunction.cpp
@@ -30,23 +30,37 @@ void GameWindow::search()
                 chess_number[i][j][1]=1;
                 chess_number[i][j][2]=1;
                 chess_number[i][j][3]=1;
-                for (int k = 0; k < 8; k ++)
+                for (int k = 0; k < 4; k ++)
                 {
-                    for (int l = 1; l < 6; l ++)
+                    for (int p = -5; p <= 0; p ++)
                     {
-                        int dx = i + f[k][0] * l;
-                        int dy = j + f[k][1] * l;
-                        if (dx<0||dy<0||dx>=20||dy>=20||
-                           (chess[dx][dy]!=chess[i][j]&&chess[dx][dy]!=0))
+                        int number = 0;
+                        int dx = i + f[k][0] * p;
+                        int dy = j + f[k][1] * p;
+                        if (dx<0||dy<0||dx>=20||dy>=20)continue;
+                        //
+                        for (int l = 0; l < 6; l ++)
                         {
-                            break;
+                            if (dx<0||dy<0||dx>=20||dy>=20)break;
+                            if (chess[dx][dy] == chess[i][j])
+                                number++;
+                            if (chess[dx][dy] == 9 - chess[i][j])
+                            {
+                                if (l + p < 0)
+                                    number = 0;
+                                break;
+                            }
+                            //if (k==0)qDebug() << dx <<' '<< dy;
+                            dx += f[k][0];
+                            dy += f[k][1];
                         }
-                        if (chess[dx][dy]==chess[i][j])
+                        if (number > chess_number[i][j][k])
                         {
-                            chess_number[i][j][k%4]++;
-                            //qDebug()<<dx<<' '<<dx<<' '<<k%4<<' '<<chess_number[i][j][k%4];
+                            chess_number[i][j][k]=number;
                         }
                     }
+                    //qDebug() <<i<<' '<<j<<' '<<k<<' '<<chess_number[i][j][k];
+
                 }
             }
         }
@@ -76,33 +90,7 @@ int GameWindow::check_ok(int x,int y)
     }
     if (nextchess == 0&&temp5 >= 2)
     {
-        int temp = 0;
-        for (int i = 0; i < 4; i ++)
-        {
-            if (chess_number[x][y][i] == 5)
-            {
-                int number = 0;
-                for (int j = 1; j < 6; j ++)
-                {
-                    int dx = x + f[i][0] * j;
-                    int dy = y + f[i][1] * j;
-                    if (dx<0||dy<0||dx>=20||dy>=20||chess[dx][dy]==5)
-                        break;
-                    if (chess[dx][dy]==0) number ++;
-                }
-                for (int j = 1; j < 6; j ++)
-                {
-                    int dx = x + f[i+4][0] * j;
-                    int dy = y + f[i+4][1] * j;
-                    if (dx<0||dy<0||dx>=20||dy>=20||chess[dx][dy]==5)
-                        break;
-                    if (chess[dx][dy]==0) number ++;
-                }
-                if (number>0) temp ++;
-            }
-        }
-        if (temp >= 2)
-            return 2;
+        return 2;
     }
     if (nextchess == 0&&temp4 >= 2)
     {
@@ -111,59 +99,54 @@ int GameWindow::check_ok(int x,int y)
         {
             if (chess_number[x][y][i] == 4)
             {
-                int number1 = 0,number2 = 0,j;//1->不连续点数目，2->两侧空出点数目
+                int mark = 0;//1->不连续点数目，2->两侧空出点数目
                 //1的值大于1不禁手，等于1但2的值小于1不禁手，等于0但2的值小于2不禁手
-                for (j = 1; j < 6; j ++)
+                for (int j = -5; j <= 0; j ++)
                 {
                     int dx = x + f[i][0] * j;
                     int dy = y + f[i][1] * j;
-                    if (dx<0||dy<0||dx>=20||dy>=20||chess[dx][dy]==5)
-                        break;
+                    if (dx<0||dy<0||dx>=20||dy>=20) continue;
+                    int flag = 0,ok = 1;
+                    for (int k = 0; k < 6; k++)
+                    {
+                        if (dx<0||dy<0||dx>=20||dy>=20) break;
+                        if (chess[dx][dy] == chess[x][y])
+                            flag ++;
+                        if (chess[dx][dy] == 9 - chess[x][y])
+                            ok = 0;
+                        dx += f[i][0];
+                        dy += f[i][1];
+                    }
+                    if (ok==0||flag<4||dx<0||dy<0||dx>=20||dy>=20)
+                        continue;
+                    if (chess[dx-f[i][0]][dy-f[i][1]] == chess[x][y]&&
+                        chess[dx-2*f[i][0]][dy-2*f[i][1]] == chess[x][y]
+                            &&chess[dx][dy] == 0)
+                        ok = 2;
+                    dx = x + f[i][0] * j;
+                    dy = y + f[i][1] * j;
+                    if (ok == 2||(chess[dx][dy] == chess[x][y]&&
+                        chess[dx+f[i][0]][dy+f[i][1]] == chess[x][y]&&
+                        dx - f[i][0] >= 0&&dy - f[i][1] >= 0&&
+                        dx - f[i][0] < 20&&dy - f[i][1] < 20&&
+                        chess[dx-f[i][0]][dy-f[i][1]] == 0))
+                            ok = 2;
+                    if (ok != 2) continue;
+                    flag = 0;ok = 0;
+                    for (int k = 0; k < 6; k ++)
+                    {
+                        if (chess[dx][dy] == chess[x][y])
+                            flag ++;
+                        if (flag > 0&&flag < 4&& chess[dx][dy] == 0)
+                            ok ++;
+                        dx += f[i][0];
+                        dy += f[i][1];
+                    }
+                    if (ok > 1) continue;
+                    mark = 1;
                 }
-                for (j --; j > 0; j --)
-                {
-                    int dx = x + f[i][0] * j;
-                    int dy = y + f[i][1] * j;
-                    if (chess[dx][dy] == 0) number2++;
-                    if (chess[dx][dy] == 4) break;
-                }
-                if (number2 == 0) continue;
-                for (j --; j > 0; j --)
-                {
-                    int dx = x + f[i][0] * j;
-                    int dy = y + f[i][1] * j;
-                    if (chess[dx][dy] == 0) number1++;
-                }
-
-                for (j = 1; j < 6; j ++)
-                {
-                    int dx = x + f[i+4][0] * j;
-                    int dy = y + f[i+4][1] * j;
-                    if (dx<0||dy<0||dx>=20||dy>=20||chess[dx][dy]==5)
-                        break;
-                }
-                int number = 0;
-                for (j --; j > 0; j --)
-                {
-                    int dx = x + f[i+4][0] * j;
-                    int dy = y + f[i+4][1] * j;
-                    if (chess[dx][dy] == 0) number2++,number++;
-                    if (chess[dx][dy] == 4) break;
-                }
-                if (number == 0) continue;
-                for (j --; j > 0; j --)
-                {
-                    int dx = x + f[i+4][0] * j;
-                    int dy = y + f[i+4][1] * j;
-                    if (chess[dx][dy] == 0) number1++;
-                }
-                qDebug()<<number1<<' '<<number2;
-                if (number1>1||(number1==1&&number2<1)||(number1==0&&number2<2))
-                {
-                    continue;
-                    qDebug()<<temp;
-                }
-                temp++;
+                if (mark == 1)
+                    temp ++;
             }
         }
         if (temp >= 2)
@@ -177,23 +160,7 @@ bool GameWindow::check_victom(int x,int y)
     for (int i = 0; i < 4; i ++)
     if (chess_number[x][y][i] >= 6)
     {
-        int number = 1;
-        for (int j = 1; j < 6; j ++, number ++)
-        {
-            int dx = x + f[i][0] * j;
-            int dy = y + f[i][1] * j;
-            if (dx<0||dy<0||dx>=20||dy>=20||chess[dx][dy]==5-nextchess||chess[dx][dy]==0)
-                break;
-        }
-        for (int j = 1; j < 6; j ++, number ++)
-        {
-            int dx = x + f[i+4][0] * j;
-            int dy = y + f[i+4][1] * j;
-            if (dx<0||dy<0||dx>=20||dy>=20||chess[dx][dy]==5-nextchess||chess[dx][dy]==0)
-                break;
-        }
-        if (number >= 6)
-            return 1;
+        return 1;
     }
 
     return 0;
